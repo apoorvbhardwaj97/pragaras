f39b54cce6a26649de17c957d9e72666a8cc5e3c
bakedpot8o_hashstash <apoorv@hashstash.in>
2019-08-31 06:20:15 +0530

    made game flow scripts

---
+++ Assets/Scripts/Controller/GameFlowController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowController : MonoBehaviour
+{
+
+    #region ------------------------------------------Private Variables--------------------------------------------------------
+
+    #endregion ----------------------------------------------------------------------------------------------------------------
+
+    #region ------------------------------------------Public Variables---------------------------------------------------------
+
+    #endregion ----------------------------------------------------------------------------------------------------------------
+
+    #region ------------------------------------------Private Methods----------------------------------------------------------
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    #endregion ----------------------------------------------------------------------------------------------------------------
+
+    #region ------------------------------------------Public Methods-----------------------------------------------------------    
+    public void Scene1Triggered()
+    {
+        //triggre these from the end point gameobject 2d cllision trigger
+        //change cam postion fade in fade out , start things in scene etc
+    }
+    public void Scene2Triggered()
+    {
+        //for dialoge you can just add bubble game obj on hero and villan and pass a string to show and trigger bubble
+    }
+    public void Scene3Triggered()
+    {
+
+    }
+    public void Scene4Triggered()
+    {
+
+    }
+    public void Scene5Triggered()
+    {
+
+    }
+    public void Scene6Triggered()
+    {
+
+    }
+    public void Scene7Triggered()
+    {
+
+    }
+    #endregion ----------------------------------------------------------------------------------------------------------------
+
+}
